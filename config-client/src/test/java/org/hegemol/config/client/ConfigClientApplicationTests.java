73d6de33800a119a3a1b9590a6591abb5b55b840
KevinClair <704714211@qq.com>
2022-05-15 01:12:43 +0800

    add new module.

---
+++ config-client/src/test/java/org/hegemol/config/client/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hegemol.config.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
