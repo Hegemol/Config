18ddcf6d22f703d1b4b277eb8ecefadddfa54ce9
KevinClair <704714211@qq.com>
2022-05-17 02:09:40 +0800

    新增常量类

---
+++ config-common/src/main/java/org/hegemol/config/common/constant/Constants.java
@@ -0,0 +1,14 @@
+package org.hegemol.config.common.constant;
+
+/**
+ * 常量枚举
+ *
+ * @author KevinClair
+ **/
+public class Constants {
+
+    /**
+     * 服务端配置监听
+     */
+    public static final String SERVER_LISTENER_URL = "/listener";
+}
