6265a3a4d6b9691b45842fa6fbdd663856c3e49f
KevinClair <704714211@qq.com>
2022-05-17 02:53:39 +0800

    增加bean注入

--- config-client/src/main/java/org/hegemol/config/client/HttpLongPollingAutoConfiguration.java
+++ config-client/src/main/java/org/hegemol/config/client/HttpLongPollingAutoConfiguration.java
@@ -3,7 +3,9 @@ package org.hegemol.config.client;
 import org.hegemol.config.client.config.HttpLongPollingConfigurationProperties;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
@@ -23,12 +25,26 @@ public class HttpLongPollingAutoConfiguration {
      * @param configurationProperties 配置类 {@link HttpLongPollingConfigurationProperties}
      * @return {@link RestTemplate}
      */
+    @Bean
     public RestTemplate restTemplate(HttpLongPollingConfigurationProperties configurationProperties) {
         OkHttp3ClientHttpRequestFactory factory = new OkHttp3ClientHttpRequestFactory();
         factory.setConnectTimeout(configurationProperties.getConnectionTimeout());
         factory.setReadTimeout(configurationProperties.getReadTimeout());
         factory.setWriteTimeout(configurationProperties.getWriteTimeout());
         return new RestTemplate(factory);
+    }
+
+    /**
+     * 初始化http长轮询请求服务
+     *
+     * @param restTemplate            rest模板
+     * @param configurationProperties 配置文件
+     * @param environment             环境变量
+     * @return Http长轮询服务 {@link HttpLongPollingService}
+     */
+    @Bean
+    public HttpLongPollingService httpLongPollingService(RestTemplate restTemplate, HttpLongPollingConfigurationProperties configurationProperties, ConfigurableEnvironment environment) {
+        return new HttpLongPollingService(restTemplate, configurationProperties, environment.getProperty("spring.application.name"));
     }
 }
 
