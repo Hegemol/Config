dca2839781e878305ae659812c7658cda4079374
KevinClair <704714211@qq.com>
2022-05-22 19:06:02 +0800

    服务端配置文件

--- config-server/src/main/java/org/hegemol/config/server/ConfigServerApplication.java
+++ config-server/src/main/java/org/hegemol/config/server/ConfigServerApplication.java
@@ -1,9 +1,11 @@
 package org.hegemol.config.server;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "org.hegemol.config.server.mapper")
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
