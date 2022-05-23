ea6277f4f978d574a7c6b80248befcc82fefe8cf
KevinClair <704714211@qq.com>
2022-05-23 20:21:54 +0800

    增加Http长轮询控制器

---
+++ config-server/src/main/java/org/hegemol/config/server/controller/HttpLongPollingController.java
@@ -0,0 +1,43 @@
+package org.hegemol.config.server.controller;
+
+import org.hegemol.config.common.model.Result;
+import org.hegemol.config.server.handler.HttpLongPollingHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 配置监听处理器
+ *
+ * @author KevinClair
+ **/
+@RestController
+public class HttpLongPollingController {
+
+    private final HttpLongPollingHandler handler;
+
+    public HttpLongPollingController(final HttpLongPollingHandler handler) {
+        this.handler = handler;
+    }
+
+    /**
+     * 配置监听
+     *
+     * @param request 请求对象
+     */
+    @PostMapping("/listener")
+    public void listener(HttpServletRequest request) {
+        handler.listener(request);
+    }
+
+    /**
+     * 初始化配置数据
+     *
+     * @param request 请求对象
+     */
+    @PostMapping("/init")
+    public Result<String> init(HttpServletRequest request) {
+        return Result.success(handler.init(request));
+    }
+}
