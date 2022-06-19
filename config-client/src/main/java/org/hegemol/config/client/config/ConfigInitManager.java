6c991feda18859ebb4eb2bbc00cb65bd98c6c647
KevinClair <704714211@qq.com>
2022-06-19 12:55:01 +0800

    rename file.

--- config-client/src/main/java/org/hegemol/config/client/config/ConfigInitManager.java
+++ config-client/src/main/java/org/hegemol/config/client/config/ConfigInitManager.java
@@ -2,6 +2,8 @@ package org.hegemol.config.client.config;
 
 import com.alibaba.fastjson2.JSON;
 import org.hegemol.config.client.annodation.ConfigValue;
+import org.hegemol.config.client.event.ConfigChangeManager;
+import org.hegemol.config.common.model.ConfigChangeFieldModel;
 import org.hegemol.config.common.model.LocalCacheClientData;
 import org.hegemol.config.common.utils.FieldReflectionUtil;
 import org.springframework.beans.BeanWrapper;
@@ -16,11 +18,11 @@ import java.util.Map;
 import java.util.Objects;
 
 /**
- * TODO
+ * 配置初始化管理器
  *
  * @author KevinClair
  **/
-public class ConfigRefreshManager implements InstantiationAwareBeanPostProcessor, BeanNameAware {
+public class ConfigInitManager implements InstantiationAwareBeanPostProcessor, BeanNameAware {
 
     private String beanName;
 
@@ -41,37 +43,43 @@ public class ConfigRefreshManager implements InstantiationAwareBeanPostProcessor
                     Map configMap = JSON.parseObject(groupValue, Map.class);
                     String confValue = (String) configMap.getOrDefault(configValue.value(), configValue.defaultValue());
 
-                    // resolves placeholders
-                    refreshBeanField(propertyName, confValue, bean);
+                    // 刷新field
+                    ConfigChangeFieldModel configChangeFieldModel = new ConfigChangeFieldModel(bean, propertyName);
+                    refreshBeanField(configChangeFieldModel, confValue);
+
+                    // 如果当前配置key需要动态刷新，添加配置变更管理器
+                    if (configValue.refresh()) {
+                        ConfigChangeManager.addChangeField(configValue.group(), configValue.value(), configChangeFieldModel);
+                    }
                 }
             });
         }
         return true;
     }
 
-    private void refreshBeanField(final String propertyName, final String confValue, final Object bean) {
-        if (Objects.isNull(bean)) {
+    private void refreshBeanField(final ConfigChangeFieldModel configChangeFieldModel, final String confValue) {
+        if (Objects.isNull(configChangeFieldModel.getBean())) {
             return;
         }
-        BeanWrapper beanWrapper = new BeanWrapperImpl(bean);
+        BeanWrapper beanWrapper = new BeanWrapperImpl(configChangeFieldModel.getBean());
 
         // property descriptor
         PropertyDescriptor propertyDescriptor = null;
         PropertyDescriptor[] propertyDescriptors = beanWrapper.getPropertyDescriptors();
         if (propertyDescriptors != null && propertyDescriptors.length > 0) {
             for (PropertyDescriptor item : propertyDescriptors) {
-                if (propertyName.equals(item.getName())) {
+                if (configChangeFieldModel.getPropertyName().equals(item.getName())) {
                     propertyDescriptor = item;
                 }
             }
         }
 
         if (Objects.nonNull(propertyDescriptor) && Objects.nonNull(propertyDescriptor.getWriteMethod())) {
-            beanWrapper.setPropertyValue(propertyName, confValue);
+            beanWrapper.setPropertyValue(configChangeFieldModel.getPropertyName(), confValue);
         } else {
-            final Object finalBean = bean;
-            ReflectionUtils.doWithFields(bean.getClass(), fieldItem -> {
-                if (propertyName.equals(fieldItem.getName())) {
+            final Object finalBean = configChangeFieldModel.getBean();
+            ReflectionUtils.doWithFields(configChangeFieldModel.getBean().getClass(), fieldItem -> {
+                if (configChangeFieldModel.getPropertyName().equals(fieldItem.getName())) {
                     try {
                         Object valueObj = FieldReflectionUtil.parseValue(fieldItem.getType(), confValue);
                         fieldItem.setAccessible(true);
