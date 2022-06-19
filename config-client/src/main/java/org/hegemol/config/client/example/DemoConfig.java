3de2b6b37629c83187327966b9c892bb3dfe9a90
KevinClair <704714211@qq.com>
2022-06-19 12:55:24 +0800

    增加配置刷新

--- config-client/src/main/java/org/hegemol/config/client/example/DemoConfig.java
+++ config-client/src/main/java/org/hegemol/config/client/example/DemoConfig.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DemoConfig {
 
-    @ConfigValue("testKey")
+    @ConfigValue(value = "testKey", refresh = true)
     private String testKey;
 
     /**
