ef292d4b32754072ff2458a519e9800b24b30d36
KevinClair <704714211@qq.com>
2022-06-26 21:39:12 +0800

    测试配置刷新test case

--- config-client/src/test/java/org/hegemol/config/client/ConfigClientApplicationTests.java
+++ config-client/src/test/java/org/hegemol/config/client/ConfigClientApplicationTests.java
@@ -1,13 +1,28 @@
 package org.hegemol.config.client;
 
+import org.hegemol.config.common.model.ConfigChangeFieldModel;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.ReflectionUtils;
 
-@SpringBootTest
 class ConfigClientApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        ConfigChangeFieldModel configChangeFieldModel = new ConfigChangeFieldModel();
+        configChangeFieldModel.setPropertyName("1111111");
+        ReflectionUtils.doWithFields(configChangeFieldModel.getClass(), fieldItem -> {
+            if (fieldItem.getName().equals("propertyName")) {
+                fieldItem.setAccessible(true);
+                fieldItem.set(configChangeFieldModel, "222222222");
+            }
+        });
+
+
+        System.out.println(configChangeFieldModel.getPropertyName());
+
+        ConfigChangeFieldModel configChangeFieldModel2 = new ConfigChangeFieldModel();
+        System.out.println(configChangeFieldModel2.getPropertyName());
+
     }
 
 }
