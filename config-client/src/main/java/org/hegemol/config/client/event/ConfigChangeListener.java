0d31a56c3b94b643ea3d4d8debfa419da97a82bb
KevinClair <704714211@qq.com>
2022-07-02 22:35:12 +0800

    增加配置监听的配置项，以及有配置发生变更时调用方法

--- config-client/src/main/java/org/hegemol/config/client/event/ConfigChangeListener.java
+++ config-client/src/main/java/org/hegemol/config/client/event/ConfigChangeListener.java
@@ -2,14 +2,23 @@ package org.hegemol.config.client.event;
 
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONObject;
+import org.hegemol.config.client.annodation.ChangeListener;
+import org.hegemol.config.client.model.ChangeListenerModel;
 import org.hegemol.config.common.model.ConfigChangeFieldModel;
 import org.hegemol.config.common.utils.FieldReflectionUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.ApplicationListener;
 import org.springframework.util.ReflectionUtils;
 
 import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
@@ -18,7 +27,10 @@ import java.util.Objects;
  *
  * @author KevinClair
  **/
-public class ConfigChangeListener implements ApplicationListener<ConfigChangeEvent> {
+public class ConfigChangeListener implements ApplicationListener<ConfigChangeEvent>, BeanPostProcessor {
+
+    private static final Logger logger = LoggerFactory.getLogger(ConfigChangeListener.class);
+    private List<ChangeListenerModel> configListeners = new ArrayList();
 
     @Override
     public void onApplicationEvent(final ConfigChangeEvent event) {
@@ -34,6 +46,29 @@ public class ConfigChangeListener implements ApplicationListener<ConfigChangeEve
                 refreshBeanField(entry.getValue(), configJsonObject.getString(entry.getKey()));
             }
         }
+        // 调用配置监听方法
+        for (ChangeListenerModel listener : configListeners) {
+            if (listener.getGroup().equals(event.getGroup())) {
+                try {
+                    listener.getMethod().invoke(listener.getObject(), event.getConfig());
+                } catch (Exception e) {
+                    logger.error("配置监听失败", e);
+                }
+            }
+        }
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(final Object bean, final String beanName) throws BeansException {
+        Method[] methods = bean.getClass().getMethods();
+        for (Method method : methods) {
+            // 监听所有
+            if (method.isAnnotationPresent(ChangeListener.class)) {
+                ChangeListener listener = method.getAnnotation(ChangeListener.class);
+                configListeners.add(new ChangeListenerModel(bean, method, listener.group()));
+            }
+        }
+        return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
     }
 
     private void refreshBeanField(final ConfigChangeFieldModel configChangeFieldModel, final String confValue) {
